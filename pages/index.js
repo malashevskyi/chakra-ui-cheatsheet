4c381bf14509d36958b8f6191b012d7f29a20355
malashevskyi <shevsky1488@gmail.com>
2021-03-09 23:25:33 +0200

    rm font link

--- pages/index.js
+++ pages/index.js
@@ -76,10 +76,6 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       {/* TOP LINKS */}
